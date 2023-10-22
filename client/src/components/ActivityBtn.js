3253a91a258003def55696d0338a00ee271ac561
priscillalynn <plynn9036@gmail.com>
2023-10-21 23:25:11 -0400

    add updateCount

--- client/src/components/ActivityBtn.js
+++ client/src/components/ActivityBtn.js
@@ -6,8 +6,17 @@ const ActivityBtn = (props) =>  {
   const [num_votes, setNumVotes] = useState(props.num_votes)
 
   const updateCount = () => {
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ num_votes: num_votes + 1 }),
+    };
 
-  }
+    fetch("/api/activities/" + props.id, options);
+    setNumVotes((num_votes) => num_votes + 1);
+  };
 
   return (
     <button className='activityBtn' id={props.id} onClick={updateCount}>
