a6d4842c8d8272426899098113efb67615169aaf
priscillalynn <plynn9036@gmail.com>
2023-10-31 11:50:52 -0400

    create login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = (props) => {
+  const AUTH_URL = `${props.api_url}/auth/github`;
+
+  return (
+    <div className="Login">
+      <h1>On the Fly ✈️</h1>
+      <center>
+        <a href={AUTH_URL}>
+          <button className="headerBtn"> 🔒 Login via Github </button>
+        </a>
+      </center>
+    </div>
+  );
+};
+
+export default Login;
